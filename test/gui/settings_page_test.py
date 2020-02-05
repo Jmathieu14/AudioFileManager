aad7f32cad72ae550692f05d4cf9e6673d5550f2
Jacques Mathieu <jmathfb@gmail.com>
2020-02-04 22:24:30 -0500

    added more tests for settings page and the new way to implement said views

--- test/gui/settings_page_test.py
+++ test/gui/settings_page_test.py
@@ -7,17 +7,38 @@ from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt
 import sys
 import unittest
-import settings_gui
+import src.view_controllers.settings_controller as settings_controller
 
 test_app = QApplication(sys.argv)
 
 class SettingsPageTest(unittest.TestCase):
 
     def setUp(self):
-        self.settings_page = settings_gui.SettingsView()
+        self.settings_page = settings_controller.SettingsView()
+        self.ui = self.settings_page.ui
 
     def test_should_have_title(self):
-        self.assertEquals(self.settings_page.ui.settings, "settings")
+        self.assertEquals(self.ui.settings.text(), "Settings")
+
+    def test_should_have_edits_folder_input_label_and_button(self):
+        self.assertIsNotNone(self.ui.editsPathEdit)
+        self.assertIsNotNone(self.ui.editsLabel)
+        self.assertEquals(self.ui.editsLabel.text(), "Edits:")
+        self.assertIsNotNone(self.ui.editsSearchBtn)
+
+    def test_should_have_downloads_folder_input_label_and_button(self):
+        self.assertIsNotNone(self.ui.dlDirectPathEdit)
+        self.assertIsNotNone(self.ui.dlLabel)
+        self.assertEquals(self.ui.dlLabel.text(), "Downloads:")
+        self.assertIsNotNone(self.ui.dlSearchBtn)
+
+    def test_should_have_destination_folder_input_label_and_button(self):
+        self.assertIsNotNone(self.ui.destPathEdit)
+        self.assertIsNotNone(self.ui.destLabel)
+        self.assertEquals(self.ui.destLabel.text(), "Destination:")
+        self.assertIsNotNone(self.ui.destSearchBtn)
+
+#     TODO: add test that ensures path on view is not replaced on file dialog close
 
 
 def get_suite():
