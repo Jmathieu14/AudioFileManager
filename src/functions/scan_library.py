98dacec2742767de4942e472c9a015355e64714c
Jacques Mathieu <jmathfb@gmail.com>
2024-01-02 14:55:40 -0500

    Initial implementation of library scan method; not tested

--- src/functions/scan_library.py
+++ src/functions/scan_library.py
@@ -1,8 +1,17 @@
+from src.models.audio_file import AudioFile
 import utility
+import os.path as osp
+
+
+audio_file_extensions = utility.file_to_json_obj(osp.abspath("../../ext/audio_codecs.json"))
 
 def scan_library(directory: str):
     if utility.does_file_exist(directory):
-        directory_map = utility.map_folder_and_subfolders(directory)
-        # flatten directory map?
-        # add each file as `AudioFile` type
-        # add artists and genres to working db
+        flattened_file_list = utility.flatten_folder(directory)
+        audio_files = []
+        for filepath in flattened_file_list:
+            if osp.splitext(filepath) in audio_file_extensions:
+                audio_file = AudioFile(filepath)
+                audio_files.append(audio_file)
+    for audio_file in audio_files:
+        print(audio_file.metadata)
